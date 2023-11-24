dbe610693fa31b351089d2ecc0bb06385cda551d
EMMANUEL <83466358+Emmanuel19-code@users.noreply.github.com>
2023-11-24 05:48:16 +0000

    emma verification

--- src/components/Emailverify.jsx
+++ src/components/Emailverify.jsx
@@ -1,9 +1,28 @@
+/* eslint-disable react/no-unescaped-entities */
 
 
 const Emailverify = () => {
   return (
-    <div>Emailverify</div>
-  )
+    <div className="w-96 bg-white shadow-lg p-4 cursor-pointer rounded-lg">
+        <div>
+            
+        </div>
+      <div>
+        <p className="text-center font-bold m-1 text-lg">
+          Verify your email using the link sent to johndoe@gmail.com
+        </p>
+        <p className="text-center text-xs text-gray-400 m-1 mt-2">
+          Didn't get any email ? check your spam folder
+        </p>
+        <p className="text-center text-gray-400 m-1">Or</p>
+        <div className="flex items-center justify-center">
+          <p className="text-blue-300 m-1">Resend Link</p>
+          <p className="text-blue-300 m-1">/</p>
+          <p className="text-blue-300 m-1">Change email</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Emailverify
